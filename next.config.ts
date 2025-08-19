1a80a485b64dd2ceddcd78b540c5deddbe3e1a58
akicaster <aki.caster@gmail.com>
2025-08-19 18:05:23 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/nextjs-gh-pages-demo',
+  assetPrefix: '/nextjs-gh-pages-demo',
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
